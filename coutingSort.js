88ddae62e42e042cc93ff6461794b1f2000865d7
AJTJ <janke.aaron@gmail.com>
2023-03-28 17:43:07 -0600

    added counting sort

---
+++ coutingSort.js
@@ -0,0 +1,8 @@
+/*
+Assumptions
+- It assumes foreknowledge that all values will be within a range
+- It assumes that all numbers will be real numbers
+- NOT comparison-based. It utilizes hashing.
+
+https://www.geeksforgeeks.org/counting-sort/
+*/
